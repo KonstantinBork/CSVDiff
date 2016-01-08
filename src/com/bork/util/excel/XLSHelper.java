9287ac7c869c5e0fbe652ccf5a2423d0eed4c791
Konstantin Bork <konstantin.bork@gmail.com>
2016-01-08 15:04:27 +0100

    Organizing the sheet contents in sets instead of lists so that removing the data is done properly.

--- src/com/bork/util/excel/XLSHelper.java
+++ src/com/bork/util/excel/XLSHelper.java
@@ -12,6 +12,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /**
  * CSVDiff
@@ -44,10 +45,10 @@ public class XLSHelper implements Helper {
             HSSFSheet sheet1 = oldWorkbook.getSheetAt(0);
             HSSFSheet sheet2 = newWorkbook.getSheetAt(0);
             Logger.log("Getting content of sheet 1");
-            List<List<String>> sheet1Contents = ExcelHelper.getSheetContents(oldWorkbook, sheet1);
+            Set<List<String>> sheet1Contents = ExcelHelper.getSheetContents(oldWorkbook, sheet1);
             controller.setProgress(FILE1PROGRESS);
             Logger.log("Getting content of sheet 2");
-            List<List<String>> sheet2Contents = ExcelHelper.getSheetContents(newWorkbook, sheet2);
+            Set<List<String>> sheet2Contents = ExcelHelper.getSheetContents(newWorkbook, sheet2);
             controller.setProgress(FILE2PROGRESS);
             sheet2Contents.removeAll(sheet1Contents);
             controller.setProgress(0.99);
@@ -58,10 +59,10 @@ public class XLSHelper implements Helper {
         }
     }
 
-    private boolean writeToNewFile(File file, List<List<String>> content) {
+    private boolean writeToNewFile(File file, Set<List<String>> content) {
         Workbook wb = new HSSFWorkbook();
         Sheet sheet = wb.createSheet();
-        Iterator<List<String>> listIterator = content.listIterator();
+        Iterator<List<String>> listIterator = content.iterator();
         int i = 0;
         return ExcelHelper.writeFile(listIterator, sheet, i, file, wb);
     }
